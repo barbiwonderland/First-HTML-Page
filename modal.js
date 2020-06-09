4991d30d0c581436296239a0e4b3b05f00e6c7b1
hotelufo <66577149+hotelufo@users.noreply.github.com>
2020-06-09 20:20:28 -0300

    Update and rename galeria.js to modal.js

---
+++ modal.js
@@ -0,0 +1,14 @@
+$(".galeria img").click(function(e){
+    var img = e.target.src;
+    var modal = '<div class="modal"><img src="'+ img + '" class="modalimg"><div class="modalboton" id= "modalboton">X</div></div>'
+    $("body").append( modal)
+$('#modalboton').click(function(){
+$('modal').remove()
+})
+
+});
+$(dcocument).keyup(function(e){
+    if (e.which==27) {
+   $('modal') .remove();
+}
+})
