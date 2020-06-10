491bb8cafe06910506a4e769a9a68963a30c266b
hotelufo <66577149+hotelufo@users.noreply.github.com>
2020-06-09 23:01:29 -0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -3,12 +3,12 @@ $(".galeria img").click(function(e){
     var modal = '<div class="modal"><img src="'+ img + '" class="modalimg"><div class="modalboton" id= "modalboton">X</div></div>'
     $("body").append( modal)
 $('#modalboton').click(function(){
-$('modal').remove()
+$('.modal').remove()
 })
 
 });
 $(dcocument).keyup(function(e){
     if (e.which==27) {
-   $('modal') .remove();
+   $('.modal') .remove();
 }
 })
